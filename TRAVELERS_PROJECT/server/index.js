7cd42e4e71585cc2ef21ecd3196b76b5cf47b0d2
NisarLDev <HarusBadah@proton.me>
2023-05-23 07:14:16 +0200

    Connected to Mongodb. First step

--- TRAVELERS_PROJECT/server/index.js
+++ TRAVELERS_PROJECT/server/index.js
@@ -8,3 +8,13 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// Connection to mongodb templete
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { userNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .chatch(() => )
